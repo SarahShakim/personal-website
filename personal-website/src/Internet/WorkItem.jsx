3207484ae6577dada137a94708cc7704167fad6a
Sarah Shakim <sshakim@uwaterloo.ca>
2025-11-04 18:25:42 -0500

    added spacing between points

--- personal-website/src/Internet/WorkItem.jsx
+++ personal-website/src/Internet/WorkItem.jsx
@@ -7,7 +7,7 @@ export default function WorkItem({ workItems }) {
                 <div key={index} className="mt-6">
                     <a className="text-lg font-semibold text-[#6c5ce7]">{job.title}</a>
                     <div className="text-base text-black/60 font-mono">{job.company} [{job.timePeriod}]</div>
-                    <ul className="list-disc ml-5 text-sm text-black/80 mt-2 space-y-1">
+                    <ul className="list-disc ml-5 text-sm text-black/80 mt-2 space-y-4">
                         {job.points.map((point, i) => (
                             <li key={i}>{point}</li>
                         ))}
