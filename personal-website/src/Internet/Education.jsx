1db78b9dedcf08dab2a72e4672bf5896ddb5a2f2
Sarah Shakim <sshakim@uwaterloo.ca>
2025-11-04 18:23:23 -0500

    tweaked design

--- personal-website/src/Internet/Education.jsx
+++ personal-website/src/Internet/Education.jsx
@@ -2,14 +2,14 @@ import React, { useEffect, useMemo, useRef, useState } from "react";
 
 export default function Education() {
     return (
-        <div className="bg-[#eafaf1] border-2 border-[#72d79e] rounded-md p-4 flex flex-col justify-center w-[300px] h-[205px]">
-                <div className="text-2xl font-extrabold text-[#6c5ce7] underline decoration-4">
+        <div className="bg-[#91EEC9] p-4 flex flex-col justify-center w-[300px] h-[205px]">
+                <div className="text-3xl font-extrabold text-[#6c5ce7] underline decoration-4">
                     EDUCATION
                 </div>
 
             <div className="mt-4 grid grid-cols-[72px_1fr] gap-5 items-start">
                 {/* year badge */}
-                <div className="bg-[#f7c2a7] border-2 border-[#f08e39] rounded-md h-28 grid place-items-center">
+                <div className="bg-[#f7c2a7] h-28 grid place-items-center">
                     <div className="flex flex-col items-center font-extrabold text-3xl leading-none text-black">
                         <span>20</span><span>24</span>
                     </div>
