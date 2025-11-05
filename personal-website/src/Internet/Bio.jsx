75ae5f61e7760ed13d6fe28489e14aac325efe7a
Sarah Shakim <sshakim@uwaterloo.ca>
2025-11-04 20:49:25 -0500

    adjusted sizes of bio section

--- personal-website/src/Internet/Bio.jsx
+++ personal-website/src/Internet/Bio.jsx
@@ -6,7 +6,7 @@ import Education from "./Education";
 export default function Bio() {
     return (
         <div className="bg-[#f7c2a7] p-6">
-            <div className="grid grid-cols-1 lg:grid-cols-[0.8fr_1.1fr_1fr] gap-6">
+            <div className="grid grid-cols-1 xl:grid-cols-[0.8fr_1.5fr_1fr] gap-6">
                 <Contact />
                 <AboutMe />
                 <div className="flex items-center justify-center ml-4">
