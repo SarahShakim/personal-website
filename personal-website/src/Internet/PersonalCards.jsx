d59eff71d5bba9fcc3cdf49af9afbcce2a77549f
Sarah Shakim <sshakim@uwaterloo.ca>
2025-11-04 21:09:02 -0500

    added spacing

--- personal-website/src/Internet/PersonalCards.jsx
+++ personal-website/src/Internet/PersonalCards.jsx
@@ -4,7 +4,7 @@ export default function PersonalCards ({title, listItems, additionalCard = false
     return (
         <div className={`bg-white/70 p-4 rounded-lg border ${additionalCard ? "mt-6" : ""}`}>
             <h3 className="text-xl font-extrabold">{title}</h3>
-            <ul className="list-disc ml-5 mt-3 space-y-1">
+            <ul className="list-disc ml-5 mt-3 space-y-2 text-sm">
                 {listItems.map((item, i) => (
                     <li key={i}>{item}</li>
                 ))}
