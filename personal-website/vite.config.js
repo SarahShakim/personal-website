127b2c3aef3b563c1ce0f9f0aab50bd370bae1a6
Sarah Shakim <sshakim@uwaterloo.ca>
2025-11-09 15:47:45 -0500

    changed params to work on website

--- personal-website/vite.config.js
+++ personal-website/vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ command }) =>({
   plugins: [react()],
-  base: '/personal-website/',
-  base: command === 'build' ? '/personal-website/' : '/', // '/' in dev
+  base: '/'
 }))
