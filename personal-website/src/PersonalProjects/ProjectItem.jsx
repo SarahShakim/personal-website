dd85cdca0a791ec230eac0f6990d030c7dad1edf
Sarah Shakim <sshakim@uwaterloo.ca>
2025-11-10 20:07:49 -0500

    added better project logos

--- personal-website/src/PersonalProjects/ProjectItem.jsx
+++ personal-website/src/PersonalProjects/ProjectItem.jsx
@@ -1,43 +1,45 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import pythonLogo from "../assets/skills/python.png"
+import KnocKnockImg from "../assets/projects/knock.png"
+import KeyboardImg from "../assets/projects/hands-free-keyboard.png"
+import ThirdEyeImg from "../assets/projects/thirdeye.png"
 
 export default function ProjectItem() {
     const projects = [
         {
             name: "ThirdEye",
             url: "https://github.com/SarahShakim/ThirdEye", 
-            icon: pythonLogo
+            icon: ThirdEyeImg
         }, 
         {
             name: "Hands-Free-Keyboard", 
             url: "https://github.com/SarahShakim/Hands-Free-Keyboard", 
-            icon: pythonLogo
+            icon: KeyboardImg
         },
         {
             name: "KnocKnock", 
             url: "https://github.com/SarahShakim/KnocKnock", 
-            icon: pythonLogo
+            icon: KnocKnockImg
         }
     ]
 
     return (
-        <div className="p-4">
+        <div className="p-4 flex flex-wrap gap-4">
             {projects.map((project, i) => (
                 <a
                     key={i}
                     href={project.url}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="inline-flex flex-col items-center gap-2"
+                    className="inline-flex w-20 flex-col items-center gap-2"
                 >
-                    <div className="w-20 h-20 rounded-full bg-black grid place-items-center">
+                    <div className="w-20 h-20 bg-black grid place-items-center">
                     <img
                         src={project.icon}
                         alt="Python"
-                        className="w-10 h-10"
+                        className="w-20 h-20"
                     />
                     </div>
-                    <span className="text-[11px] text-black/70">{project.name}</span>
+                    <span className="text-center break-words text-[13px] text-black/70">{project.name}</span>
                 </a>
             ))}
         </div>
